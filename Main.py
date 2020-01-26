39d81a3beeb39f6428c4ecbf9a9c6b09b9f64ce0
Paweł <pawelb021@gmail.com>
2020-01-26 21:47:42 +0100

    Made MainGraph widget own class

--- Main.py
+++ Main.py
@@ -14,20 +14,24 @@ from kivy.properties import NumericProperty, StringProperty, ObjectProperty
 from math import sin, cos
 
 
-class MainWindow(BoxLayout):
-    # Graph variables
-    graph = ObjectProperty(None)
+class MainGraph(Graph):
+    # Graph Properties
     xlabel_name = StringProperty('X')
     ylabel_name = StringProperty('Y')
-    xmin = NumericProperty(-10)
-    xmax = NumericProperty(10)
-    ymin = NumericProperty(-2)
-    ymax = NumericProperty(2)
+    xmin = NumericProperty(-3)
+    xmax = NumericProperty(3)
+    ymin = NumericProperty(-10)
+    ymax = NumericProperty(10)
 
     def update_graph(self):
         plot = SmoothLinePlot(color=[1, 0, 0, 1])
-        plot.points = [(x, sin(x)) for x in range(self.xmin, self.xmax + 1)]
-        self.graph.add_plot(plot)
+        plot.points = [(x / 10, sin(x / 10)) for x in range(self.xmin * 10, self.xmax * 10 + 1)]
+        self.add_plot(plot)
+
+
+class MainWindow(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MainWindow, self).__init__(**kwargs)
 
 
 class GraphCalcApp(App):
